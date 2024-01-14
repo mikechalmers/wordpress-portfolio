bee6bbf65c99e973187cd3e38e91557bab965542
mikechalmers <mikechalmers@gmail.com>
2024-01-14 13:29:08 +0000

    use attachment ID in sliders

--- partials/new-single-slider.php
+++ partials/new-single-slider.php
@@ -1,21 +1,25 @@
 <div class="slideshow-container">
 
     <?php
-        $fields = CFS()->get( 'images' );
-        foreach ( $fields as $field ) { ?>
-            <div class="mySlides fade">
-                <img src="<?php echo $field['image']; ?>" class="slide" testing alt="<?php echo $field['alt_text']; ?>" />
-            </div>
-            
-        <?php }
+    $fields = CFS()->get('images');
+    foreach ($fields as $field) {
+        $image_id = $field['image'];
+        $image_url = wp_get_attachment_image_src($image_id, 'full')[0];
+        $alt_text = get_post_meta($image_id, '_wp_attachment_image_alt', TRUE);
+        ?>
+                    <div class="mySlides fade">
+                        <img src="<?php echo $image_url; ?>" class="slide" alt="<?php echo $alt_text; ?>" />
+                    </div>
+                
+            <?php }
     ?>
 
-    <?php if ( !empty( get_the_content() ) ): ?>
-        <div class="mySlides fade">
-            <div class="blurb">
-                <?php the_content(); ?>
+    <?php if (!empty(get_the_content())): ?>
+            <div class="mySlides fade">
+                <div class="blurb">
+                    <?php the_content(); ?>
+                </div>
             </div>
-        </div>
     <?php endif; ?>
 
     <a class="prev" onclick="plusSlides(-1)"><img class="direction" src="<?php bloginfo('template_directory'); ?>/images/left.png" /></a>
