6fd178413f3eb11b3b21925d4509568d994767e8
mikechalmers <mikechalmers@gmail.com>
2024-01-14 15:44:33 +0000

    add titles

--- partials/new-single-slider.php
+++ partials/new-single-slider.php
@@ -22,6 +22,8 @@
             </div>
     <?php endif; ?>
 
+    <h1><?php the_title(); ?></h1>
+
     <a class="prev" onclick="plusSlides(-1)"><img class="direction" src="<?php bloginfo('template_directory'); ?>/images/left.png" alt="Previous Image" /></a>
     <a class="next" onclick="plusSlides(1)"><img class="direction" src="<?php bloginfo('template_directory'); ?>/images/right.png" alt="Next Image" /></a>
 </div>
